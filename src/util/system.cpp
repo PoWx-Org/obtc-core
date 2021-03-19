d37e33d7480f1dd7b1248d225e1cf75d5fd8d922
Borland04 <McGarret160497@gmail.com>
2021-03-19 21:57:09 +0300

    Change datadir 'Bitcoin' -> 'Obtc'

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -561,7 +561,7 @@ fs::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Obtc";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -571,10 +571,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Obtc";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".obtc";
 #endif
 #endif
 }
