1dcb71c1fe2aa59bfa1d49eacfd289d2fef32566
Anton Kovalov <anton.kov.dev@gmail.com>
2022-07-13 14:28:34 +0200

    Refactors powcache implementation
    1. Adds a wrapper around PoW cache database, which allows for easy substitution of any heavy block.GetHash() encounter.
    2. Removes redundant calls.
    
    Consideration:
    Performing a database lookup with 160 bit long buffer instead of 256 bit blob.
    Due to the fact that SHA1 output is 160 bits long 256 - 160 = 96 bits are zeros simply taking up space. It is not a big issue, but may result in extra 1 MB of data every week.

--- src/powcache.cpp
+++ src/powcache.cpp
@@ -4,21 +4,47 @@
 
 #include <powcache.h>
 
-PowCacheDB pow_cache(GetDataDir(false) / "powcache", 4194304, false, false);
-
-PowCacheDB::PowCacheDB(fs::path ldb_path, size_t nCacheSize, bool fMemory, bool fWipe) :
+CPowCacheDB::CPowCacheDB(fs::path ldb_path, size_t nCacheSize, bool fMemory, bool fWipe) :
     db(ldb_path, nCacheSize, fMemory, fWipe, true)
 {}
 
-bool PowCacheDB::HaveCacheEntry(const uint256& lookupHash) const {
-    return db.Exists(lookupHash);
+Optional<uint256> CPowCacheDB::GetCacheEntry(const uint256& lookupHash) const {
+    uint256 blockHash;
+    if (db.Read(lookupHash, blockHash)) return blockHash;
+
+    return nullopt;
 }
 
-bool PowCacheDB::GetCacheEntry(const uint256& lookupHash, uint256& powHash) const {
-    return db.Read(lookupHash, powHash);
-}
-
-bool PowCacheDB::WriteCacheEntry(const uint256& lookupHash, uint256& powHash) {
+bool CPowCacheDB::WriteCacheEntry(const uint256& lookupHash, const uint256& powHash) {
     return db.Write(lookupHash, powHash);
 }
 
+void CPowHashProxy::Init(size_t nCacheSize)
+{
+    pow_cachedb.reset();
+    pow_cachedb.reset(new CPowCacheDB(GetDataDir(false) / "powcache", nCacheSize, false, false));
+};
+
+void CPowHashProxy::Stop()
+{
+    pow_cachedb.reset();
+};
+
+uint256 CPowHashProxy::GetHash(CBlockHeader& block)
+{
+    if(pow_cachedb)
+    {
+        uint256 cacheHash = block.GetCacheHash();
+        
+        if (Optional<uint256> optionalBlockHash = pow_cachedb->GetCacheEntry(cacheHash))
+            return optionalBlockHash.get();
+        
+        uint256 blockHash = block.GetHash();
+        pow_cachedb->WriteCacheEntry(cacheHash, blockHash);
+        return blockHash;
+    }
+
+    return block.GetHash();
+};
+
+CPowHashProxy powHashProxy;
--- src/powcache.h
+++ src/powcache.h
@@ -6,39 +6,44 @@
 #define BITCOIN_POWCACHE_H
 
 #include <dbwrapper.h>
+#include <optional.h>
+#include <primitives/block.h>
 #include <uint256.h>
 
-/** Implements a persistent hash lookup cache, using SHA1 hash as the key
- * allowing instant retrieval of the more cpu-expensive pow hash if known.
+/** Implements a persistent hash lookup cache, using SHA1 hash as the key.
+ * If hash was previosuly encountered and stored it allows for instant retrieval of the more cpu-expensive pow HeavyHash.
  * The hash is not tied to a height, preventing invalid hashes from potentially
  * being returned in the instance of a block reorganisation etc.
  */
-class PowCacheDB
+
+//! Max memory allocated for HeavyHash cache (4 MiB ~ 4.2 MB)
+static const int64_t nMaxHeavyHashCache = 4;
+
+class CPowCacheDB
 {
 protected:
     CDBWrapper db;
 
-private:
-    int hit_log{0};
-    int miss_log{0};
-
 public:
-    explicit PowCacheDB(fs::path ldb_path, size_t nCacheSize, bool fMemory, bool fWipe);
-
-    int hit(bool inc = false) {
-        if (inc) ++hit_log;
-        return hit_log;
-    }
-    int miss(bool inc = false) {
-        if (inc) ++miss_log;
-        return miss_log;
-    }
-
-    bool HaveCacheEntry(const uint256& lookupHash) const;
-    bool GetCacheEntry(const uint256& lookupHash, uint256& powHash) const;
-    bool WriteCacheEntry(const uint256& lookupHash, uint256& powHash);
+    explicit CPowCacheDB(fs::path ldb_path, size_t nCacheSize, bool fMemory, bool fWipe);
+    
+    // Read method already checks for record existence
+    Optional<uint256> GetCacheEntry(const uint256& lookupHash) const;
+    bool WriteCacheEntry(const uint256& lookupHash, const uint256& powHash);
 };
 
-extern PowCacheDB pow_cache;
+class CPowHashProxy
+{
+protected:
+    std::unique_ptr<CPowCacheDB> pow_cachedb;
+
+public:
+    void Init(size_t nCacheSize);
+    void Stop();
+    uint256 GetHash(CBlockHeader& block);
+};
+
+// Global wrapper around CPoWCacheDB object. It is responsible for its existence, and based on this it decides how to retrieve block's hash
+extern CPowHashProxy powHashProxy;
 
 #endif // BITCOIN_POWCACHE_H
